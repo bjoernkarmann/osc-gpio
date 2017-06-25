81074e8f61b5fc19d0c6e660aebcfbb8d8ca8460
Bjørn Karmann <bjoern.t.karmann@gmail.com>
2017-06-25 19:01:35 +0200

    new gpio library

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-//var gpio = require("pi-gpio");
+var rpio = require('rpio');
+var pin = 12;
+
+rpio.open(pin, rpio.OUTPUT, rpio.LOW);
 
 var osc = require('osc-min'),
     dgram = require('dgram'),
     remote;
+
 
 // listen for OSC messages and print them to the console
 var udp = dgram.createSocket('udp4', function(msg, rinfo) {
@@ -13,23 +17,11 @@ var udp = dgram.createSocket('udp4', function(msg, rinfo) {
   try {
     if(osc.fromBuffer(msg).address=="/off"){
       console.log("Relay Off");
-
-      // set gpio pin 17 to LOW
-      gpio.open(17, "output", function(err) {
-        gpio.write(17, 0, function() {
-            gpio.close(16);
-        });
-      });
+      rpio.write(pin, rpio.HIGH);
     }
     if(osc.fromBuffer(msg).address=="/on"){
       console.log("Relay ON");
-
-      // set gpio pin 17 to HIGH
-      gpio.open(17, "output", function(err) {
-        gpio.write(17, 1, function() {
-            gpio.close(16);
-        });
-      });
+      rpio.write(pin, rpio.LOW);
     }
   } catch (err) {
     console.log('Could not decode OSC message');
